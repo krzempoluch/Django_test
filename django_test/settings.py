fa298c515d2787eb97d45421375a7dbdd99f3642
krzempoluch <krzempoluch@metoda.com.pl>
2014-11-12 13:47:34 +0100

    Revert "Revert "Merge commit '43a35d6a0aed871202c9c8e5be66c46dd1a37bf7'""
    This reverts commit 261780f72b29792003744c325426bee3b3e2dcd1.

--- django_test/settings.py
+++ django_test/settings.py
@@ -62,10 +62,10 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd1knmip8kstsuj',
-        'USER': 'rreelctgawyrrk',
-        'PASSWORD': 'J2KIo7cshlryZP2WwMBTKTLOJW',
-        'HOST': 'ec2-54-163-249-168.compute-1.amazonaws.com',
+        'NAME': 'ress',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
