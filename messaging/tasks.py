0e2045935779ec1cca551342aee14ef906bf8dfe
krzempoluch <rzempoluch.konrad@gmail.com>
2014-11-17 17:08:14 +0100

    jkghjhgkj

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -4,7 +4,7 @@ from projekty.queue import PublishQueue
 
 app = Celery('tasks', backend='amqp', broker='amqp://iypkanhf:f7W5aI8SOzDje6BM-e-JSPcR4k7V7VFh@turtle.rmq.cloudamqp.com/iypkanhf')
 logger = logging.getLogger(__name__+'Task')
-producer = PublishQueue('reportReturnQueue', 'amqp://iypkanhf:f7W5aI8SOzDje6BM-e-JSPcR4k7V7VFh@turtle.rmq.cloudamqp.com/iypkanhf')
+producer = PublishQueue('reportReturnQueue')
 
 @app.task
 def gen_report(id):
