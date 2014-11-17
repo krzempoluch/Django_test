e09492f0032cf8611492f30f24fd6842314e16e1
krzempoluch <rzempoluch.konrad@gmail.com>
2014-11-17 16:22:34 +0100

    Dodanie adresów cloudAMQP do kolejek, poprawka

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -2,9 +2,9 @@ from celery import Celery
 import logging, time, datetime, os
 from projekty.queue import PublishQueue
 
-app = Celery('tasks', backend='amqp', broker=os.environ['cloudamqp_56497'])
+app = Celery('tasks', backend='amqp', broker='amqp://iypkanhf:f7W5aI8SOzDje6BM-e-JSPcR4k7V7VFh@turtle.rmq.cloudamqp.com/iypkanhf')
 logger = logging.getLogger(__name__+'Task')
-producer = PublishQueue('reportReturnQueue', os.environ['cloudamqp_56497'])
+producer = PublishQueue('reportReturnQueue', 'amqp://iypkanhf:f7W5aI8SOzDje6BM-e-JSPcR4k7V7VFh@turtle.rmq.cloudamqp.com/iypkanhf')
 
 @app.task
 def gen_report(id):
