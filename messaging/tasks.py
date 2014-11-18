86c6f3f64e55e03447a86f2081e12f78f657b749
krzempoluch <krzempoluch@metoda.com.pl>
2014-11-18 15:36:27 +0100

    poprawka

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 import logging, time, datetime, os
-from queue.queue import PublishQueue
+from messaging.queue import PublishQueue
 
 app = Celery('tasks', backend='amqp', broker='amqp://iypkanhf:f7W5aI8SOzDje6BM-e-JSPcR4k7V7VFh@turtle.rmq.cloudamqp.com/iypkanhf')
 logger = logging.getLogger(__name__+'Task')
