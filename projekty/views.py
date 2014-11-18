6a6723e7af8dced89aef814eacd4a2d4ce376a39
krzempoluch <krzempoluch@metoda.com.pl>
2014-11-18 11:47:19 +0100

    Kolejka odbierajaca raporty startuje na poczatku aplikacji

--- projekty/views.py
+++ projekty/views.py
@@ -10,8 +10,6 @@ from projekty.queue import ReportConsumerQueue
 import logging, os
 
 logger = logging.getLogger(__name__+'views')
-subscriber = ReportConsumerQueue('reportReturnQueue', 'amqp://iypkanhf:f7W5aI8SOzDje6BM-e-JSPcR4k7V7VFh@turtle.rmq.cloudamqp.com:5672/iypkanhf')
-subscriber.consume()
 
 # Create your views here.
 def index(request):
